5a99364e69864b3aa0c14113bcded9e47fe91ac0
funonon <funo222@outlook.jp>
2024-10-26 12:25:40 +0900

    ver1.1.2.2　バグ修正

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import keep_alive
+import asyncio
 
 # インテントを設定
 intents = discord.Intents.default()
@@ -9,13 +10,20 @@ intents.messages = True  # メッセージ関連のインテントを有効化
 
 TOKEN = os.getenv('TOKEN')
 
-bot = commands.Bot(command_prefix='!', intents=intents)  # intentsを渡す
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.tree.sync()  # スラッシュコマンドを同期
     print(f'Logged in as {bot.user}')
+
+async def load_extensions():
+    await bot.load_extension('じゃんけん')  # じゃんけんコマンドのエクステンションをロード
 
 # Botの実行
 if __name__ == "__main__":
     keep_alive.keep_alive()  # Flaskサーバーを起動
-    bot.run(TOKEN)  # Botを実行
+
+    # イベントループ内でbotとエクステンションを非同期起動
+    asyncio.run(load_extensions())
+    bot.run(TOKEN)
